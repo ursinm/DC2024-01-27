d5a1f84e2ee385ffede48014698fe39886576496
Paval-from-Belarus <star-war-sandakan@mail.ru>
2024-01-31 12:20:35 +0300

    add calculator-parent; add rest-application

---
+++ 151001/Shlyk/SpringCalculator/spring-calculator-rest/src/main/java/by/bsuir/poit/dc/rest/SpringCalculatorRestApplication.java
@@ -0,0 +1,15 @@
+package by.bsuir.poit.dc.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Paval Shlyk
+ * @since 31/01/2024
+ */
+@SpringBootApplication
+public class SpringCalculatorRestApplication {
+    public static void main(String[] args) {
+	SpringApplication.run(SpringCalculatorRestApplication.class, args);
+    }
+}
