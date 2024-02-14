9a0ccb673e2cfcc9bbc3564b80e2cd01d33b44b6
Tishalovich Maxim <tishalovichm@gmail.com>
2024-02-14 22:55:07 +0300

    Add error controller

---
+++ 151004/Tishalovich/rest_basics/src/main/java/by/bsuir/dc/rest_basics/controllers/ErrorController.java
@@ -0,0 +1,20 @@
+package by.bsuir.dc.rest_basics.controllers;
+
+import by.bsuir.dc.rest_basics.services.exceptions.ApiException;
+import by.bsuir.dc.rest_basics.services.exceptions.ApiExceptionInfo;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ErrorController {
+
+    @ExceptionHandler( ApiException.class )
+    public ResponseEntity<ApiExceptionInfo> handleApiException(ApiException e) {
+        ApiExceptionInfo apiExceptionInfo = e.getApiExceptionInfo();
+        return new ResponseEntity<>(apiExceptionInfo,
+                HttpStatusCode.valueOf(apiExceptionInfo.getHttpStatusCode()));
+    }
+
+}
