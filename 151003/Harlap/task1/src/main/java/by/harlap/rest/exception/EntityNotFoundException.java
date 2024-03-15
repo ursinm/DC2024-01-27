cc6adb647724efc78b7a7be854bc3b21d08a0f07
Diana Harlap <dianaharlap004@gmail.com>
2024-03-15 21:41:51 +0300

    Implement task 1

---
+++ 151003/Harlap/task1/src/main/java/by/harlap/rest/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package by.harlap.rest.exception;
+
+
+public class EntityNotFoundException extends RuntimeException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ 151003/Harlap/task1/src/test/java/by/harlap/rest/facade/TagFacadeTest.java
@@ -0,0 +1,144 @@
+package by.harlap.rest.facade;
+
+import by.harlap.rest.dto.request.CreateTagDto;
+import by.harlap.rest.dto.request.UpdateTagDto;
+import by.harlap.rest.dto.response.TagResponseDto;
+import by.harlap.rest.mapper.TagMapper;
+import by.harlap.rest.model.Tag;
+import by.harlap.rest.service.TagService;
+import by.harlap.rest.util.dto.request.CreateTagDtoTestBuilder;
+import by.harlap.rest.util.dto.request.UpdateTagDtoTestBuilder;
+import by.harlap.rest.util.dto.response.TagResponseDtoTestBuilder;
+import by.harlap.rest.util.entity.DetachedTagTestBuilder;
+import by.harlap.rest.util.entity.TagTestBuilder;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+class TagFacadeTest {
+
+    @InjectMocks
+    private TagFacade tagFacade;
+
+    @Mock
+    private TagService tagService;
+
+    @Mock
+    private TagMapper tagMapper;
+
+    @Test
+    @DisplayName("Test should return expected response")
+    void findById() {
+        final Tag persistedTag = TagTestBuilder.tag().build();
+        final TagResponseDto expected = TagResponseDtoTestBuilder.tag().build();
+
+        doReturn(persistedTag)
+                .when(tagService)
+                .findById(persistedTag.getId());
+
+        doReturn(expected)
+                .when(tagMapper)
+                .toTagResponse(persistedTag);
+
+        final TagResponseDto actual = tagFacade.findById(expected.getId());
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Test should return expected response")
+    void findAll() {
+        final Tag persistedTag = TagTestBuilder.tag().build();
+        final TagResponseDto expectedTag = TagResponseDtoTestBuilder.tag().build();
+        final List<TagResponseDto> expected = List.of(expectedTag);
+
+        doReturn(List.of(persistedTag))
+                .when(tagService)
+                .findAll();
+
+        doReturn(expectedTag)
+                .when(tagMapper)
+                .toTagResponse(persistedTag);
+
+        final List<TagResponseDto> actual = tagFacade.findAll();
+
+        assertThat(actual).isEqualTo(expected);
+
+    }
+
+    @Test
+    @DisplayName("Test should return expected response")
+    void save() {
+        final Tag detachedTag = DetachedTagTestBuilder.tag().build();
+        final CreateTagDto request = CreateTagDtoTestBuilder.tag().build();
+        final Tag persistedTag = TagTestBuilder.tag().build();
+        final TagResponseDto expected = TagResponseDtoTestBuilder.tag().build();
+
+        doReturn(detachedTag)
+                .when(tagMapper)
+                .toTag(request);
+
+        doReturn(persistedTag)
+                .when(tagService)
+                .save(detachedTag);
+
+        doReturn(expected)
+                .when(tagMapper)
+                .toTagResponse(persistedTag);
+
+        final TagResponseDto actual = tagFacade.save(request);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Test should return expected response")
+    void update() {
+        final UpdateTagDto request = UpdateTagDtoTestBuilder.tag().build();
+        final Tag persistedTag = TagTestBuilder.tag().build();
+        final TagResponseDto expected = TagResponseDtoTestBuilder.tag().build();
+
+        doReturn(persistedTag)
+                .when(tagService)
+                .findById(persistedTag.getId());
+
+        doReturn(persistedTag)
+                .when(tagMapper)
+                .toTag(request, persistedTag);
+
+        doReturn(persistedTag)
+                .when(tagService)
+                .update(persistedTag);
+
+        doReturn(expected)
+                .when(tagMapper)
+                .toTagResponse(persistedTag);
+
+        final TagResponseDto actual = tagFacade.update(request);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("Test should complete successfully for provided id")
+    void delete() {
+        final Long tagId = 1L;
+
+        doNothing().when(tagService)
+                .deleteById(tagId);
+
+        tagFacade.delete(tagId);
+
+        verify(tagService, times(1)).deleteById(tagId);
+    }
+}
