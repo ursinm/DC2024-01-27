0aa4c2054ca4015bf6632ef81b9c776047b39d72
kanura56@gmail.com <kanura56@gmail.com>
2024-03-17 19:16:58 +0300

    lab1

---
+++ 151002/Haiduk/RV/RV/Infrastructure/Validators/NewsRequestDtoValidator.cs
@@ -0,0 +1,14 @@
+﻿using Api.DTO.RequestDTO;
+using FluentValidation;
+
+namespace Api.Infrastructure.Validators
+{
+    public class NewsRequestDtoValidator : AbstractValidator<NewsRequestDto>
+    {
+        public NewsRequestDtoValidator()
+        {
+            RuleFor(dto => dto.Title).Length(2, 64);
+            RuleFor(dto => dto.Content).Length(4, 2048);
+        }
+    }
+}
---
+++ 151002/Haiduk/RV/RV/Models/Note.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Models
+{
+    public class Note
+    {
+        public long Id { get; set; }
+
+        public long NewsId { get; set; }
+
+        public News News { get; set; }
+
+        public string Content { get; set; }
+    }
+}
