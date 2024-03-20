7fbc7d770703327ee5d8856d3db007382cffbdfc
iamnovik <vlad09082003@mail.ru>
2024-03-20 20:10:17 +0300

    Lab 2

---
+++ 151001/Novik/Lab2/REST/Models/Entity/Post.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Numerics;
+
+namespace REST.Models.Entity;
+
+public class Post
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public long id { get; set; }
+    
+    [ForeignKey("newsId")]
+    public long? newsId { get; set; }
+    
+    [StringLength(2048, MinimumLength = 2)]
+    public String? content { get; set; }
+    
+    public News News { get; set; }
+}
