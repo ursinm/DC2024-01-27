18ff28f38ffe426addbd1a171730e866b7effb69
Chernika_56 <bosko.working@mail.ru>
2024-04-01 21:36:53 +0300

    lab1

---
+++ 151004/Bosko/lab1/lab1/DTO/UserRequestTo.cs
@@ -0,0 +1,17 @@
+﻿using lab1.DTO.Interface;
+
+namespace lab1.DTO
+{
+    public class UserRequestTo(int Id, string? Login, string? Password, string? Firstname, string? Lastname) : IRequestTo
+    {
+        public int Id { get; set; } = Id;
+
+        public string? Login { get; set; } = Login;
+
+        public string? Password { get; set; } = Password;
+
+        public string? Firstname { get; set; } = Firstname;
+
+        public string? Lastname { get; set; } = Lastname;
+    }
+}
