a5153ed918c1f21001a03f33c545c060856549c1
Tishalovich Maxim <tishalovichm@gmail.com>
2024-04-03 12:49:38 +0300

    Feat: discussion module service and controller layers.

--- 151004/Tishalovich/rest_basics/discussion/src/main/java/by/bsuir/dc/discussion/controller/MessageController.java
+++ 151004/Tishalovich/rest_basics/discussion/src/main/java/by/bsuir/dc/discussion/controller/MessageController.java
@@ -3,25 +3,50 @@ package by.bsuir.dc.discussion.controller;
 import by.bsuir.dc.discussion.entity.MessageRequestTo;
 import by.bsuir.dc.discussion.entity.MessageResponseTo;
 import by.bsuir.dc.discussion.service.MessageService;
+import by.bsuir.dc.discussion.service.exception.ApiException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
+@RequestMapping("api/v1.0/messages")
 @RequiredArgsConstructor
 public class MessageController {
 
-    private final MessageService messageService;
+    private final MessageService service;
 
-    @PostMapping("/messages")
-    public MessageResponseTo create(@RequestBody MessageRequestTo requestTo) {
-        return messageService.create(requestTo);
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public MessageResponseTo createMessage(
+            @RequestBody MessageRequestTo requestTo) throws ApiException {
+
+        return service.create(requestTo);
     }
 
-    @GetMapping("/messages")
-    public Iterable<MessageResponseTo> getAll() {
-        return messageService.getAll();
+    @GetMapping("")
+    public List<MessageResponseTo> getMessages() throws ApiException {
+        return service.getAll();
     }
+
+    @GetMapping("{id}")
+    public MessageResponseTo getMessage(@PathVariable Long id) throws ApiException {
+        return service.get(id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void deleteMessage(@PathVariable(value = "id") Long id)
+            throws ApiException {
+
+        service.delete(id);
+    }
+
+    @PutMapping("")
+    public MessageResponseTo updateMessage(@RequestBody MessageRequestTo message)
+            throws ApiException {
+
+        return service.update(message);
+    }
+
 }
--- 151004/Tishalovich/rest_basics/discussion/src/main/java/by/bsuir/dc/discussion/service/MessageService.java
+++ 151004/Tishalovich/rest_basics/discussion/src/main/java/by/bsuir/dc/discussion/service/MessageService.java
@@ -4,13 +4,19 @@ import by.bsuir.dc.discussion.dal.MessageDao;
 import by.bsuir.dc.discussion.entity.Message;
 import by.bsuir.dc.discussion.entity.MessageRequestTo;
 import by.bsuir.dc.discussion.entity.MessageResponseTo;
+import by.bsuir.dc.discussion.service.exception.ApiException;
+import by.bsuir.dc.discussion.service.exception.GeneralSubCode;
+import by.bsuir.dc.discussion.service.exception.MessageSubCode;
 import by.bsuir.dc.discussion.service.mapper.MessageMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 @RequiredArgsConstructor
@@ -20,22 +26,72 @@ public class MessageService {
 
     private final MessageMapper mapper;
 
-    public MessageResponseTo create(MessageRequestTo requestTo) {
-        Message message = mapper.requestToModel(requestTo);
-        message.setId(new Random().nextLong());
-        Message saved = dao.save(message);
-        return mapper.modelToResponse(saved);
+    //I think that random have no issues with synchronization
+    private final Random random = new Random();
+
+    public MessageResponseTo create(MessageRequestTo requestTo) throws ApiException {
+        Message model = mapper.requestToModel(requestTo);
+        model.setId(random.nextLong());
+        Message savingRes;
+        try {
+            savingRes = dao.save(model);
+        } catch (DataIntegrityViolationException e) {
+            throw new ApiException(
+                    HttpStatus.FORBIDDEN.value(),
+                    MessageSubCode.CONSTRAINT_VIOLATION.getSubCode(),
+                    e.getMessage()
+            );
+        }
+        return mapper.modelToResponse(savingRes);
     }
 
-    public Iterable<MessageResponseTo> getAll() {
-        List<MessageResponseTo> res = new ArrayList<>();
+    public List<MessageResponseTo> getAll() {
+        Iterable<Message> models = dao.findAll();
+        return StreamSupport
+                .stream(models.spliterator(), false)
+                .map(mapper::modelToResponse)
+                .collect(Collectors.toList());
+    }
 
-        Iterable<Message> messages = dao.findAll();
-        for (Message message : messages) {
-            res.add(mapper.modelToResponse(message));
+    public MessageResponseTo get(Long id) throws ApiException {
+        Optional<Message> author = dao.findById(id);
+
+        if (author.isEmpty()) {
+            throw new ApiException(
+                    HttpStatus.NOT_FOUND.value(),
+                    GeneralSubCode.WRONG_ID.getSubCode(),
+                    GeneralSubCode.WRONG_ID.getMessage()
+            );
         }
 
-        return res;
+        return mapper.modelToResponse(author.get());
+    }
+
+    public MessageResponseTo update(MessageRequestTo requestTo) throws ApiException {
+        Message model = mapper.requestToModel(requestTo);
+
+        Message updateRes = dao.save(model);
+        //if (updateRes == null) {
+        //    throw new ApiException(
+        //            HttpStatus.NOT_FOUND.value(),
+        //            GeneralSubCode.WRONG_ID.getSubCode(),
+        //            GeneralSubCode.WRONG_ID.getMessage()
+        //    );
+        //}
+
+        return mapper.modelToResponse(updateRes);
+    }
+
+    public void delete(Long id) throws ApiException {
+        if (!dao.existsById(id)) {
+            throw new ApiException(
+                    HttpStatus.NOT_FOUND.value(),
+                    GeneralSubCode.WRONG_ID.getSubCode(),
+                    GeneralSubCode.WRONG_ID.getMessage()
+            );
+        }
+
+        dao.deleteById(id);
     }
 
 }
