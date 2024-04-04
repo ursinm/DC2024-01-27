74b71eab562b5f97aecc333aa59f3284401a0be9
Maxin4rm <92393477+Maxin4rm@users.noreply.github.com>
2024-04-04 10:46:19 +0300

    Add lab1

---
+++ 151002/Kuvshinov/lab1/Infrastructure/Validators/LabelRequestDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using DC.DTO.RequestDTO;
+using FluentValidation;
+
+namespace DC.Infrastructure.Validators
+{
+	public class LabelRequestDtoValidator : AbstractValidator<LabelRequestDto>
+	{
+		public LabelRequestDtoValidator()
+		{
+			RuleFor(dto => dto.Name).Length(2, 32);
+		}
+	}
+}
