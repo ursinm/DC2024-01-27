3f9f0f1bc2e6712d033ee6770a9a2d849a99c110
Andrik7289 <Aberezkin954@gmail.com>
2024-04-07 16:15:20 +0300

    JPA implemented.

--- 151001/Beryozkin/REST/src/main/java/app/services/MessageService.java
+++ 151001/Beryozkin/REST/src/main/java/app/services/MessageService.java
@@ -1,17 +1,22 @@
 package app.services;
 
-import app.dao.MessageDao;
 import app.dto.MessageRequestTo;
 import app.dto.MessageResponseTo;
 import app.entities.Message;
 import app.exceptions.DeleteException;
 import app.exceptions.NotFoundException;
 import app.exceptions.UpdateException;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
 import app.mapper.MessageListMapper;
 import app.mapper.MessageMapper;
+import app.repository.MessageRepository;
+import app.repository.TweetRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
@@ -24,35 +29,61 @@ public class MessageService {
     @Autowired
     MessageMapper messageMapper;
     @Autowired
-    MessageDao messageDao;
+    MessageRepository messageDao;
     @Autowired
     MessageListMapper messageListMapper;
+    @Autowired
+    TweetRepository tweetRepository;
 
     public MessageResponseTo getMessageById(@Min(0) Long id) throws NotFoundException {
         Optional<Message> message = messageDao.findById(id);
-        return message.map(value -> messageMapper.messageToMessageResponse(value)).orElseThrow(() -> new NotFoundException("message not found!", 40004L));
+        return message.map(value -> messageMapper.messageToMessageResponse(value)).orElseThrow(() -> new NotFoundException("Message not found!", 40004L));
     }
 
-    public List<MessageResponseTo> getmessages() {
-        return messageListMapper.toMessageResponseList(messageDao.findAll());
+    public List<MessageResponseTo> getMessages(Integer pageNumber, Integer pageSize, String sortBy, String sortOrder) {
+        Pageable pageable;
+        if (sortOrder != null && sortOrder.equals("asc")) {
+            pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortBy).ascending());
+        } else {
+            pageable = PageRequest.of(pageNumber, pageSize, Sort.by(sortBy).descending());
+        }
+        Page<Message> messages = messageDao.findAll(pageable);
+        return messageListMapper.toMessageResponseList(messages.toList());
     }
 
-    public MessageResponseTo savemessage(@Valid MessageRequestTo message) {
+    public MessageResponseTo saveMessage(@Valid MessageRequestTo message) {
         Message messageToSave = messageMapper.messageRequestToMessage(message);
+        if (message.getTweetId() != null) {
+            messageToSave.setTweet(tweetRepository.findById(message.getTweetId()).orElseThrow(() -> new NotFoundException("Tweet not found!", 40004L)));
+        }
         return messageMapper.messageToMessageResponse(messageDao.save(messageToSave));
     }
 
     public void deleteMessage(@Min(0) Long id) throws DeleteException {
-        messageDao.delete(id);
+        if (!messageDao.existsById(id)) {
+            throw new DeleteException("Message not found!", 40004L);
+        } else {
+            messageDao.deleteById(id);
+        }
     }
 
     public MessageResponseTo updateMessage(@Valid MessageRequestTo message) throws UpdateException {
         Message messageToUpdate = messageMapper.messageRequestToMessage(message);
-        return messageMapper.messageToMessageResponse(messageDao.update(messageToUpdate));
+        if (!messageDao.existsById(message.getId())) {
+            throw new UpdateException("Message not found!", 40004L);
+        } else {
+            if (message.getTweetId() != null) {
+                messageToUpdate.setTweet(tweetRepository.findById(message.getTweetId()).orElseThrow(() -> new NotFoundException("Tweet not found!", 40004L)));
+            }
+            return messageMapper.messageToMessageResponse(messageDao.save(messageToUpdate));
+        }
     }
 
-    public MessageResponseTo getMessageByTweetId(@Min(0) Long tweetId) throws NotFoundException {
-        Optional<Message> message = messageDao.getMessageByTweetId(tweetId);
-        return message.map(value -> messageMapper.messageToMessageResponse(value)).orElseThrow(() -> new NotFoundException("message not found!", 40004L));
+    public List<MessageResponseTo> getMessageByTweetId(@Min(0) Long tweetId) throws NotFoundException {
+        List<Message> message = messageDao.findMessagesByTweet_Id(tweetId);
+        if (message.isEmpty()) {
+            throw new NotFoundException("Message not found!", 40004L);
+        }
+        return messageListMapper.toMessageResponseList(message);
     }
 }
