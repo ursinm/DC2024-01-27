2f3626b4bc63ed4e55bc9cfabc993bf699d68e96
blessedboy14 <92112019+blessedboy14@users.noreply.github.com>
2024-04-09 15:31:13 +0300

    added redis, kafka, cassandra

--- 151004/Prokopchuk/lab3/cassa_stuff/cassa_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from rest_framework import routers
-
-from .views import NoteViewSet
-
-urlpatterns = [
-    path('notes', NoteViewSet.as_view(), name="notes"),
-    path('notes/<int:pk>', NoteViewSet.as_view(), name="notes_get"),
-]
