e959d00d06c7012b847340acc5f4a4334c618054
Tishalovich Maxim <tishalovichm@gmail.com>
2024-04-12 08:49:30 +0300

    Refactor: delete unnecessary content.

--- 151004/Tishalovich/rest_basics/publisher/src/main/java/by/bsuir/dc/publisher/dal/MessageDao.java
+++
@@ -1,7 +0,0 @@
-package by.bsuir.dc.publisher.dal;
-
-import by.bsuir.dc.publisher.entities.Message;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MessageDao extends CrudRepository<Message, Long> {
-}
