c15a6f8c2dcf5cbc1868cc37e665977d50e29d44
Tishalovich Maxim <tishalovichm@gmail.com>
2024-04-12 09:05:52 +0300

    Feat: change message controller to extract locale.

--- 151004/Tishalovich/rest_basics/publisher/src/main/java/by/bsuir/dc/publisher/controllers/MessageController.java
+++ 151004/Tishalovich/rest_basics/publisher/src/main/java/by/bsuir/dc/publisher/controllers/MessageController.java
@@ -2,18 +2,52 @@ package by.bsuir.dc.publisher.controllers;
 
 import by.bsuir.dc.publisher.entities.dtos.request.MessageRequestTo;
 import by.bsuir.dc.publisher.entities.dtos.response.MessageResponseTo;
+import by.bsuir.dc.publisher.services.MessageService;
 import by.bsuir.dc.publisher.services.common.AbstractService;
 import by.bsuir.dc.publisher.controllers.common.AbstractController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import by.bsuir.dc.publisher.services.exceptions.ApiException;
+import by.bsuir.dc.publisher.services.impl.MessageServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1.0/messages")
-public class MessageController extends AbstractController<MessageRequestTo, MessageResponseTo> {
+@RequiredArgsConstructor
+public class MessageController {
 
-    public MessageController(AbstractService<MessageRequestTo,
-                MessageResponseTo> service) {
-        super(service);
+    private final MessageServiceImpl service;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("")
+    public MessageResponseTo createMessage(
+            @RequestBody MessageRequestTo requestTo,
+            @RequestHeader("Accept-Language") String lang) throws ApiException {
+
+        return service.create(requestTo, lang);
+    }
+
+    @GetMapping("")
+    public List<MessageResponseTo> getMessages() throws ApiException {
+        return service.getAll();
+    }
+
+    @GetMapping("{id}")
+    public MessageResponseTo getMessage(@PathVariable Long id) throws ApiException {
+        return service.get(id);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("{id}")
+    public void deleteMessage(@PathVariable(value = "id") Long id) throws ApiException {
+        service.delete(id);
+    }
+
+    @PutMapping("")
+    public MessageResponseTo updateMessage(@RequestBody MessageRequestTo message) throws ApiException {
+        return service.update(message);
     }
 
 }
