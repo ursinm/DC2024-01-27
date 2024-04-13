99b7fcf4085f74eff71c8fe1d596f02e2b05807e
Никита Глушаченко <rylonoboev2004@gmail.com>
2024-04-13 18:58:13 +0300

    lab3 done

---
+++ 151004/Glushachenko/lab3/cassandra/Publisher/Controllers/V1_0/AuthorController.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Publisher.Controllers.V1_0.Common;
+using Publisher.Entity.Db;
+using Publisher.Entity.DTO.RequestTO;
+using Publisher.Entity.DTO.ResponseTO;
+using Publisher.Service.Interface;
+using System.Net;
+
+namespace Publisher.Controllers.V1_0
+{
+    [Route("api/v1.0/authors")]
+    [ApiController]
+    public class AuthorController(IAuthorService AuthorService, ILogger<AuthorController> Logger, IMapper Mapper) :
+        AbstractController<Author, AuthorRequestTO, AuthorResponseTO>(AuthorService, Logger, Mapper)
+    {
+        [HttpGet]
+        [Route("tweets/{id:int}")]
+        public async Task<JsonResult> GetByTweetID([FromRoute] int id)
+        {
+            AuthorResponseTO? response = null;
+            Logger.LogInformation("Getting author by tweet ID: {id}", id);
+
+            try
+            {
+                response = await AuthorService.GetByTweetID(id);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError("Error getting author by tweet ID {ex}", ex);
+                Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+
+            return Json(response);
+        }
+    }
+}
