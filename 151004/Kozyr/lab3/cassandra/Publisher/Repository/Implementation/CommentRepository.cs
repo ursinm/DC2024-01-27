ff74e60a3c634e98557bf52752e24c4acc568470
Kozyr <oralcumshot677@proton.me>
2024-04-13 19:49:23 +0300

    lab3 done

---
+++ 151004/Kozyr/lab3/cassandra/Publisher/Repository/Implementation/CommentRepository.cs
@@ -0,0 +1,30 @@
+﻿using Publisher.Entity.Db;
+using Publisher.Repository.Implementation.Common;
+using Publisher.Repository.Interface;
+using Publisher.Storage.Common;
+
+namespace Publisher.Repository.Implementation
+{
+    public class CommentRepository(DbStorage context) : AbstractCrudRepository<Comment>(context), ICommentRepository
+    {
+        private readonly DbStorage _context = context;
+
+        public override Comment Add(Comment comment)
+        {
+            var issue = new Issue { Id = comment.IssueId };
+            _context.Issues.Attach(issue);
+            comment.Issue = issue;
+
+            return base.Add(comment);
+        }
+
+        public override async Task<Comment> AddAsync(Comment comment)
+        {
+            var issue = new Issue { Id = comment.IssueId };
+            _context.Issues.Attach(issue);
+            comment.Issue = issue;
+
+            return await base.AddAsync(comment);
+        }
+    }
+}
