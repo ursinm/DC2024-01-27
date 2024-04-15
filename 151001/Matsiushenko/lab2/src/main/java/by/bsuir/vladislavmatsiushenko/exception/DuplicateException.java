62e65e0242d89d4ec966307a944ec124f67c398c
Vladislav-Matsiushenko <vladislav.matsiushenko@softera.by>
2024-04-15 13:20:51 +0300

    Matsiushenko lab 2

---
+++ 151001/Matsiushenko/lab2/src/main/java/by/bsuir/vladislavmatsiushenko/exception/DuplicateException.java
@@ -0,0 +1,13 @@
+package by.bsuir.vladislavmatsiushenko.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class DuplicateException extends RuntimeException {
+    private String message;
+    private long status;
+}
