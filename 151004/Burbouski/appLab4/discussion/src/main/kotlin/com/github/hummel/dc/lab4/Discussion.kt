b9ab3c28b3fe9389d720e2c12cc101de7848bd1a
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-16 21:58:26 +0300

    пока просто копия

---
+++ 151004/Burbouski/appLab4/discussion/src/main/kotlin/com/github/hummel/dc/lab4/Discussion.kt
@@ -0,0 +1,28 @@
+package com.github.hummel.dc.lab4
+
+import com.github.hummel.dc.lab4.controller.configureRouting
+import com.github.hummel.dc.lab4.controller.configureSerialization
+import com.github.hummel.dc.lab4.module.appModule
+import com.github.hummel.dc.lab4.module.dataModule
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.doublereceive.*
+import org.koin.ktor.plugin.Koin
+
+//вводить (Ctrl + Shift + V) в Exec Docker'а на запущенном контейнере Cassandra с 9042:9042
+//cqlsh
+//CREATE KEYSPACE distcomp WITH REPLICATION = {'class' : 'NetworkTopologyStrategy', 'datacenter1' : 3};
+//CREATE TABLE distcomp.tbl_message_by_country (country text, issue_id bigint, id bigint, content text, PRIMARY KEY ((country), issue_id, id));
+fun main() {
+	embeddedServer(Netty, port = 24130, module = Application::discussion).start(wait = true)
+}
+
+fun Application.discussion() {
+	install(DoubleReceive)
+	install(Koin) {
+		modules(dataModule, appModule)
+	}
+	configureSerialization()
+	configureRouting()
+}
---
+++ 151004/Burbouski/appLab4/publisher/src/main/kotlin/com/github/hummel/dc/lab4/Publisher.kt
@@ -0,0 +1,41 @@
+package com.github.hummel.dc.lab4
+
+import com.github.hummel.dc.lab4.controller.configureRouting
+import com.github.hummel.dc.lab4.controller.configureSerialization
+import com.github.hummel.dc.lab4.module.appModule
+import com.github.hummel.dc.lab4.module.dataModule
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.doublereceive.*
+import org.koin.ktor.plugin.Koin
+import java.sql.Connection
+import java.sql.DriverManager
+
+fun main() {
+	embeddedServer(Netty, port = 24110, module = Application::publisher).start(wait = true)
+}
+
+fun Application.publisher() {
+	install(DoubleReceive)
+	install(Koin) {
+		dataModule.single<Connection> { connectToPostgres(embedded = true) }
+		modules(dataModule, appModule)
+	}
+	configureSerialization()
+	configureRouting()
+}
+
+fun Application.connectToPostgres(embedded: Boolean): Connection {
+	Class.forName("org.postgresql.Driver")
+
+	if (embedded) {
+		return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
+	}
+
+	val url = environment.config.property("postgres.url").getString()
+	val user = environment.config.property("postgres.user").getString()
+	val password = environment.config.property("postgres.password").getString()
+
+	return DriverManager.getConnection(url, user, password)
+}
