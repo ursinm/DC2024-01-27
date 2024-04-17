e3aa6914b86971d9fe2aabe8559337ff61457a64
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-17 14:48:55 +0300

    Update Discussion.kt

--- 151004/Burbouski/appLab4/discussion/src/main/kotlin/com/github/hummel/dc/lab4/Discussion.kt
+++ 151004/Burbouski/appLab4/discussion/src/main/kotlin/com/github/hummel/dc/lab4/Discussion.kt
@@ -18,10 +18,10 @@ import java.time.Duration
 import java.util.*
 import kotlin.concurrent.thread
 
-//вводить (Ctrl + Shift + V) в Exec Docker'а на запущенном контейнере Cassandra с 9042:9042
-//cqlsh
-//CREATE KEYSPACE distcomp WITH REPLICATION = {'class' : 'NetworkTopologyStrategy', 'datacenter1' : 3};
-//CREATE TABLE distcomp.tbl_message_by_country (country text, issue_id bigint, id bigint, content text, PRIMARY KEY ((country), issue_id, id));
+//вводить в терминал
+//docker network create kafkanet
+//docker run -d --network=kafkanet --name=zookeeper -e ZOOKEEPER_CLIENT_PORT=2181 -e ZOOKEEPER_TICK_TIME=2000 -p 2181:2181 confluentinc/cp-zookeeper
+//docker run -d --network=kafkanet --name=kafka -e KAFKA_ZOOKEEPER_CONNECT=zookeeper:2181 -e KAFKA_ADVERTISED_LISTENERS=PLAINTEXT://localhost:9092 -e KAFKA_OFFSETS_TOPIC_REPLICATION_FACTOR=1 -p 9092:9092 confluentinc/cp-kafka
 
 lateinit var consumer: KafkaConsumer<String, String>
 
