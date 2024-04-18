4d937ca8cc398cd2da44d9d42df0049e606876ec
Tishalovich Maxim <tishalovichm@gmail.com>
2024-04-18 22:24:23 +0300

    Add redis label service and dao.

---
+++ 151004/Tishalovich/rest_basics/publisher/src/main/java/by/bsuir/dc/publisher/services/impl/RedisLabelService.java
@@ -0,0 +1,40 @@
+package by.bsuir.dc.publisher.services.impl;
+
+import by.bsuir.dc.publisher.dal.RedisLabelDao;
+import by.bsuir.dc.publisher.entities.dtos.response.LabelResponseTo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.StreamSupport;
+
+@Service
+@RequiredArgsConstructor
+public class RedisLabelService {
+
+    private final RedisLabelDao dao;
+
+    public void save(LabelResponseTo label) {
+        dao.save(label);
+    }
+
+    public Optional<LabelResponseTo> findById(Long id) {
+        return dao.findById(id);
+    }
+
+    public void deleteById(Long id) {
+        dao.deleteById(id);
+    }
+
+    public void update(LabelResponseTo label) {
+        dao.save(label);
+    }
+
+    public Iterable<LabelResponseTo> findAll() {
+        return StreamSupport.stream(dao.findAll().spliterator(), false)
+                .filter(Objects::nonNull)
+                .toList();
+    }
+
+}
