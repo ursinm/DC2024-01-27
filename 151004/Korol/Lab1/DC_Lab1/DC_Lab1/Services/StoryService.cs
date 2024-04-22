f858e2a964bbda47bac057cede8ced3287221573
korol <korol634921777@gmail.com>
2024-04-22 18:31:34 +0300

    Added lab1-lab5

---
+++ 151004/Korol/Lab1/DC_Lab1/DC_Lab1/Services/StoryService.cs
@@ -0,0 +1,93 @@
+﻿using AutoMapper;
+using DC_Lab1.DB.BaseDBContext;
+using DC_Lab1.DTO;
+using DC_Lab1.DTO.Interface;
+using DC_Lab1.Models;
+using DC_Lab1.Services.Interfaces;
+using Humanizer;
+using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DC_Lab1.Services
+{
+    public class StoryService(IMapper _mapper, BaseContext dbContext) : IStoryService
+    {
+        public async Task<IResponseTo> CreateEnt(IRequestTo Dto)
+        {
+            var StoryDto = (StoryRequestTo)Dto;
+            if (!Validate(StoryDto))
+            {
+                throw new InvalidDataException("Incorrect data for CREATE Story");
+            }
+            var Story = _mapper.Map<Story>(StoryDto);
+            string time = DateTime.UtcNow.ToString("o", CultureInfo.InvariantCulture);
+            Story.Created = time;
+            Story.Modified = time;
+            dbContext.Storys.Add(Story);
+            await dbContext.SaveChangesAsync();
+            var response = _mapper.Map<StoryResponseTo>(Story);
+            return response;
+        }
+
+        public async Task DeleteEnt(int id)
+        {
+            try
+            {
+                var Story = await dbContext.Storys.FindAsync(id);
+                dbContext.Storys.Remove(Story!);
+                await dbContext.SaveChangesAsync();
+                return;
+            }
+            catch
+            {
+                throw new Exception("Deletting Story exception");
+            }
+        }
+
+        public async Task<IResponseTo> GetEntById(int id)
+        {
+            var Story = _mapper.Map<StoryResponseTo>(await dbContext.Storys.FindAsync(id));
+            return Story is not null ? _mapper.Map<StoryResponseTo>(Story) : throw new ArgumentNullException($"Not found Story: {id}");
+        }
+
+        public IEnumerable<IResponseTo> GetAllEnt()
+        {
+            try
+            {
+                return dbContext.Storys.Select(_mapper.Map<StoryResponseTo>);
+            }
+            catch
+            {
+                throw new Exception("Gettting all Storys exception");
+            }
+        }
+
+        public async Task<IResponseTo> UpdateEnt(IRequestTo Dto)
+        {
+            var StoryDto = (StoryRequestTo)Dto;
+
+            if (!Validate(StoryDto))
+            {
+                throw new InvalidDataException("Incorrect data for UPDATE Story");
+            }
+            var newStory = _mapper.Map<Story>(StoryDto);
+            string time = DateTime.UtcNow.ToString("o", CultureInfo.InvariantCulture);
+            newStory.Modified = time;
+            dbContext.Storys.Update(newStory);
+            await dbContext.SaveChangesAsync();
+            var Story = _mapper.Map<StoryResponseTo>(await dbContext.Storys.FindAsync(newStory.Id));
+            return Story;
+        }
+
+        public bool Validate(StoryRequestTo StoryDto)
+        {
+            if (StoryDto?.Title?.Length < 2 || StoryDto?.Title?.Length > 64)
+                return false;
+            if (StoryDto?.Content?.Length < 8 || StoryDto?.Content?.Length > 2048)
+                return false;
+
+            return true;
+        }
+    }
+}
---
+++ 151004/Korol/Lab5/publisher/DC_Lab1/DC_Lab1/Controllers/StickerController.cs
@@ -0,0 +1,102 @@
+﻿using DC_Lab1.DTO.Interface;
+using DC_Lab1.DTO;
+using DC_Lab1.Services;
+using DC_Lab1.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DC_Lab1.Controllers
+{
+    [ApiController]
+    [Route("/api/v1.0/stickers")]
+    public class StickerController(IStickerService StickerService) : Controller
+    {
+        [HttpGet]
+        public JsonResult GetStickers()
+        {
+            try
+            {
+                var Stickers = StickerService.GetAllEnt();
+                return Json(Stickers);
+            }
+            catch
+            {
+                Response.StatusCode = 400;
+                return Json(BadRequest());
+
+            }
+
+        }
+
+        [HttpGet]
+        [Route("{stickerId:int}")]
+        public async Task<JsonResult> GetStickerById(int stickerId)
+        {
+            try
+            {
+                var Sticker = await StickerService.GetEntById(stickerId);
+                return Json(Sticker);
+            }
+            catch
+            {
+                Response.StatusCode = 400;
+                return Json(BadRequest());
+            }
+
+
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> CreateSticker(StickerRequestTo StickerTo)
+        {
+            try
+            {
+                Response.StatusCode = 201;
+                var Sticker = await StickerService.CreateEnt(StickerTo);
+                return Json(Sticker);
+            }
+            catch
+            {
+                Response.StatusCode = 400;
+                return Json(BadRequest());
+            }
+
+        }
+
+        [HttpPut]
+        public async Task<JsonResult> UpdateSticker(StickerRequestTo StickerTo)
+        {
+            IResponseTo newSticker;
+            try
+            {
+                newSticker = await StickerService.UpdateEnt(StickerTo);
+                Response.StatusCode = 200;
+                return Json(newSticker);
+
+            }
+            catch
+            {
+                Response.StatusCode = 400;
+                return Json(BadRequest());
+            }
+        }
+
+        [HttpDelete("{stickerId}")]
+        public async Task<IActionResult> DeleteSticker(int stickerId)
+        {
+            try
+            {
+                Response.StatusCode = 204;
+                await StickerService.DeleteEnt(stickerId);
+            }
+            catch
+            {
+                Response.StatusCode = 401;
+                return BadRequest();
+            }
+
+            return NoContent();
+
+
+        }
+    }
+}
