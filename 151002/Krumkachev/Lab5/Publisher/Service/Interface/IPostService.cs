35a5b2ad7155b5bede97f124183c4a9cc5cf6027
dkrumkachev <dkrumkachev@DESKTOP-DN38416>
2024-04-27 02:30:17 +0300

    add lab4, lab5

---
+++ 151002/Krumkachev/Lab5/Publisher/Service/Interface/IPostService.cs
@@ -0,0 +1,14 @@
+﻿using Publisher.Entity.DTO.RequestTO;
+using Publisher.Kafka.Model;
+
+namespace Publisher.Service.Interface
+{
+    public interface IPostService
+    {
+        Task<IList<PostKafkaResponse>> GetAll();
+        Task<PostKafkaResponse?> Add(PostRequestTO requestTo);
+        Task<bool> Remove(int id);
+        Task<PostKafkaResponse?> Update(PostRequestTO requestTo);
+        Task<PostKafkaResponse?> GetByID(int id);
+    }
+}
