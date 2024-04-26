b01c766f1ab94418f08b58360401cda835f31203
AlexeyRyabov04 <ryabovalexey04@gmail.com>
2024-04-26 11:40:29 +0300

    Add all labs

---
+++ 151002/Ryabov/DC/DC.Api/Repositories/ITagRepository.cs
@@ -0,0 +1,16 @@
+﻿using Forum.Api.Models;
+
+namespace Forum.Api.Repositories;
+
+public interface ITagRepository
+{
+    public Task<List<Tag>> GetAllAsync();
+    
+    public Task<Tag?> GetByIdAsync(long id);
+
+    public Task<Tag> CreateAsync(Tag tagModel);
+    
+    public Task<Tag?> UpdateAsync(long id, Tag updatedTag);
+    
+    public Task<Tag?> DeleteAsync(long id);
+}
