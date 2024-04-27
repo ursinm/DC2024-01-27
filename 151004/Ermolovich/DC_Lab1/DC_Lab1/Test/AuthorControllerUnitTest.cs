37071e1be2576788ebced2c1983b0f1739c7f0d0
Dmitriy <dima.ermolovich04@gmail.com>
2024-04-27 22:14:24 +0300

    Add lab 1-3

---
+++ 151004/Ermolovich/DC_Lab1/DC_Lab1/Test/AuthorControllerUnitTest.cs
@@ -0,0 +1,89 @@
+using AutoMapper;
+using DC_Lab1.DB;
+using DC_Lab1.DTO.Interface;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace Test
+{
+    [TestClass]
+    public class AuthorControllerUnitTest
+    {
+
+        private readonly AuthorController _controller;
+        private readonly AuthorService _service;
+        private readonly DC_Lab1.DB.SqLiteDbContext _dbContext;
+        private readonly IMapper _mapper = new MapperConfiguration(cfg =>
+        {
+            cfg.AddMaps(["DC_Lab1"]);
+        }).CreateMapper();
+        public AuthorControllerUnitTest()
+        { 
+            _dbContext = new DC_Lab1.DB.SqLiteDbContext();
+            _service = new AuthorService(_mapper, _dbContext);
+            _controller = new AuthorController(_service)
+            { ControllerContext = new ControllerContext { HttpContext = new DefaultHttpContext() } };
+
+
+        }
+
+        [TestMethod]
+        public void GetUpdate()
+        {
+            var AuthorRequest = new AuthorRequestTo(0,"testLogin", "password", "fname", "lname");
+            var Author = _controller.CreateAuthor(AuthorRequest).Result.Value as AuthorResponseTo;
+            Assert.IsNotNull(Author);
+
+            var newAuthor = new AuthorRequestTo(Author.Id,"newTestLogin", "newPassword", "newFname", "newLname");
+            _dbContext.ChangeTracker.Clear();
+            var AuthorResponce = _controller.UpdateAuthor(newAuthor).Result.Value as AuthorResponseTo;
+            Assert.IsNotNull(AuthorResponce);
+            Assert.AreEqual(AuthorResponce.Login, newAuthor.Login);
+        }
+        [TestMethod]
+        public void GetEntById()
+        {
+            var AuthorRequest = new AuthorRequestTo(0,"testLogin", "password", "fname", "lname");
+            var Author = _controller.CreateAuthor(AuthorRequest).Result.Value as AuthorResponseTo;
+            Assert.IsNotNull(Author);
+            _dbContext.ChangeTracker.Clear();
+            var foundedAuthor = _controller.GetAuthorById(Author.Id).Result.Value as AuthorResponseTo;
+            Assert.IsNotNull(foundedAuthor);
+            Assert.AreEqual(foundedAuthor.Id, Author.Id);
+        }
+        [TestMethod]
+        public async Task GetAllEnt()
+        {
+            
+            var result = _controller.GetAuthors();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result.Value, typeof(IEnumerable<IResponseTo>));
+        }
+        [TestMethod]
+        public async Task CreateEnt()
+        {
+            var AuthorRequest = new AuthorRequestTo(0,"testLogin", "password", "fname", "lname");
+            var AuthorResponse = (await _controller.CreateAuthor(AuthorRequest)).Value as AuthorResponseTo;
+            Assert.IsNotNull(AuthorResponse);
+
+            Assert.AreEqual(AuthorResponse.Login, "testLogin");
+        }
+        [TestMethod]
+        public async Task DeleteEnt()
+        {
+            var AuthorRequest = new AuthorRequestTo(0, "testDeleteLogin", "password", "fname", "lname");
+            var AuthorResponse = (await _controller.CreateAuthor(AuthorRequest)).Value as AuthorResponseTo;
+            Assert.IsNotNull(AuthorResponse);
+
+            _dbContext.ChangeTracker.Clear();
+
+            var result = await _controller.DeleteAuthor(AuthorResponse.Id);
+
+            var expected = typeof(NoContentResult);
+
+            Assert.IsInstanceOfType(result, expected);
+        }
+
+    }
+}
---
+++ 151004/Ermolovich/DC_Lab2/DC_Lab1/SqLiteDbContext.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using DC_Lab1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DC_Lab1;
+
+public partial class SqLiteDbContext : DbContext
+{
+    public SqLiteDbContext()
+    {
+        Database.EnsureCreated();
+    }
+
+    public SqLiteDbContext(DbContextOptions<SqLiteDbContext> options)
+        : base(options)
+    {
+        Database.EnsureCreated();
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Post> Messages { get; set; }
+
+    public virtual DbSet<Label> Stickers { get; set; }
+
+    public virtual DbSet<Story> Storys { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlite("DataSource=file::memory:?cache=shared");
+
+    //protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //{
+    //    modelBuilder.Entity<Editor>(entity =>
+    //    {
+    //        entity.ToTable("Editor");
+
+    //        entity.HasIndex(e => e.Login, "IX_Editor_login").IsUnique();
+
+    //        entity.Property(e => e.Id)
+    //            .ValueGeneratedNever()
+    //            .HasColumnName("id");
+    //        entity.Property(e => e.Firstname).HasColumnName("firstname");
+    //        entity.Property(e => e.Lastname).HasColumnName("lastname");
+    //        entity.Property(e => e.Login).HasColumnName("login");
+    //        entity.Property(e => e.Password).HasColumnName("password");
+    //    });
+
+    //    modelBuilder.Entity<Post>(entity =>
+    //    {
+    //        entity.ToTable("Post");
+
+    //        entity.Property(e => e.Id)
+    //            .ValueGeneratedNever()
+    //            .HasColumnName("id");
+    //        entity.Property(e => e.Content).HasColumnName("content");
+    //        entity.Property(e => e.storyId).HasColumnName("storyId");
+
+    //        entity.HasOne(d => d.Story).WithMany(p => p.Posts).HasForeignKey(d => d.storyId);
+    //    });
+
+    //    modelBuilder.Entity<Sticker>(entity =>
+    //    {
+    //        entity.ToTable("Sticker");
+
+    //        entity.HasIndex(e => e.Id, "IX_Sticker_id").IsUnique();
+
+    //        entity.HasIndex(e => e.Name, "IX_Sticker_name").IsUnique();
+
+    //        entity.Property(e => e.Id)
+    //            .ValueGeneratedNever()
+    //            .HasColumnName("id");
+    //        entity.Property(e => e.Name).HasColumnName("name");
+    //    });
+
+    //    modelBuilder.Entity<Story>(entity =>
+    //    {
+    //        entity.ToTable("Story");
+
+    //        entity.HasIndex(e => e.Title, "IX_Story_title").IsUnique();
+
+    //        entity.Property(e => e.Id)
+    //            .ValueGeneratedNever()
+    //            .HasColumnName("id");
+    //        entity.Property(e => e.Content).HasColumnName("content");
+    //        entity.Property(e => e.Created).HasColumnName("created");
+    //        entity.Property(e => e.Modified).HasColumnName("modified");
+    //        entity.Property(e => e.Title).HasColumnName("title");
+
+    //        entity.HasOne(d => d.Editor).WithMany(p => p.Storys).HasForeignKey(d => d.EditorId);
+    //    });
+
+    //    OnModelCreatingPartial(modelBuilder);
+    //}
+
+    //partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
