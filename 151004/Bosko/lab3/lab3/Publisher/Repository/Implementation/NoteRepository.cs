3577d3bedb277b210e0111aa752822dbde5230cf
Chernika_56 <bosko.working@mail.ru>
2024-04-29 02:18:18 +0300

    lab3

---
+++ 151004/Bosko/lab3/lab3/Publisher/Repository/Implementation/NoteRepository.cs
@@ -0,0 +1,30 @@
+﻿using Publisher.Entity.Db;
+using Publisher.Repository.Implementation.Common;
+using Publisher.Repository.Interface;
+using Publisher.Storage.Common;
+
+namespace Publisher.Repository.Implementation
+{
+    public class NoteRepository(DbStorage context) : AbstractCrudRepository<Note>(context), INoteRepository
+    {
+        private readonly DbStorage _context = context;
+
+        public override Note Add(Note note)
+        {
+            var news = new News { Id = note.NewsId };
+            _context.News.Attach(news);
+            note.News = news;
+
+            return base.Add(note);
+        }
+
+        public override async Task<Note> AddAsync(Note note)
+        {
+            var news = new News { Id = note.NewsId };
+            _context.News.Attach(news);
+            note.News = news;
+
+            return await base.AddAsync(note);
+        }
+    }
+}
