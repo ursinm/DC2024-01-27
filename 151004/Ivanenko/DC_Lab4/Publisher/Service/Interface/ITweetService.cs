4400521ac8b725c24cf6402bdcfb885c83696d11
KotVMeshke <dimon2003mail.ru37@gmail.com>
2024-04-28 22:48:00 +0300

    Assignments 3 - 5 are done

---
+++ 151004/Ivanenko/DC_Lab4/Publisher/Service/Interface/ITweetService.cs
@@ -0,0 +1,13 @@
+﻿using Publisher.Entity.Db;
+using Publisher.Entity.DTO.RequestTO;
+using Publisher.Entity.DTO.ResponseTO;
+using Publisher.Service.Interface.Common;
+
+namespace Publisher.Service.Interface
+{
+    public interface ITweetService : ICrudService<Tweet, TweetRequestTO, TweetResponseTO>
+    {
+        Task<TweetResponseTO> GetTweetByParam(IList<string> StickerNames, IList<int> StickerIds, string EditorLogin,
+            string title, string content);
+    }
+}
