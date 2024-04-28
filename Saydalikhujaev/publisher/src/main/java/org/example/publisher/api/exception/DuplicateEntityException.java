ccbd9aff19f6860590b9a9db631bf3df6a6e6fff
Saydullin <emptybasenone@gmail.com>
2024-04-28 20:47:36 +0300

    Delete Saydalikhujaev directory

--- Saydalikhujaev/publisher/src/main/java/org/example/publisher/api/exception/DuplicateEntityException.java
+++
@@ -1,7 +0,0 @@
-package org.example.publisher.api.exception;
-
-public class DuplicateEntityException extends Exception{
-    public DuplicateEntityException(String entity, String name) {
-        super(entity + " with name " + name + " already exists");
-    }
-}
--- Saydalikhujaev/publisher/src/main/java/org/example/publisher/impl/creator/CreatorRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.publisher.impl.creator;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.math.BigInteger;
-
-public interface CreatorRepository extends JpaRepository<Creator, BigInteger> {
-
-}
