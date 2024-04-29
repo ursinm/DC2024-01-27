9a0786c7743b79ee60583f3ac4e6a03f10e55598
Darya-Samusenko <92455760+Darya-Samusenko@users.noreply.github.com>
2024-04-29 18:34:52 +0300

    Add lab 3
    Now in right place

---
+++ 151002/Samusenko/lab_three/publisher/src/main/java/com/example/publisher/dao/repository/LabelRepository.java
@@ -0,0 +1,9 @@
+package com.example.publisher.dao.repository;
+
+import com.example.publisher.dao.ARepository;
+import com.example.publisher.model.entity.Label;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class LabelRepository extends ARepository<Label> {
+}
---
+++ 151002/Samusenko/lab_three/publisher/src/main/java/com/example/publisher/model/IEntity.java
@@ -0,0 +1,6 @@
+package com.example.publisher.model;
+
+public interface IEntity {
+    Long getId();
+    void setId(Long id);
+}
