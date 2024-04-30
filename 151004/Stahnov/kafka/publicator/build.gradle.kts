95ba540e52f662007726e55421787d4412adfcb1
Nikstanov <N.stahnov@gmail.com>
2024-04-30 18:44:44 +0300

    docker compose

--- 151004/Stahnov/kafka/publicator/build.gradle.kts
+++ 151004/Stahnov/kafka/publicator/build.gradle.kts
@@ -11,6 +11,5 @@ tasks.getByName<Jar>("jar") {
 dependencies{
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis:3.2.4")
-    implementation("redis.clients:jedis:5.1.2")
     runtimeOnly("org.postgresql:postgresql")
 }
--- 151004/Stahnov/kafka/publicator/src/main/java/org/education/config/RedisConfig.java
+++ 151004/Stahnov/kafka/publicator/src/main/java/org/education/config/RedisConfig.java
@@ -10,7 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -30,6 +35,15 @@ public class RedisConfig {
 
     @Value("${spring.redis.port}")
     private int redisPort;
+
+    @Bean
+    public RedisConfiguration defaultRedisConfig() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(redisHost);
+        config.setPort(redisPort);
+        config.setPassword("redis");
+        return config;
+    }
 
     @Bean
     public RedisTemplate<String, Serializable> redisCacheTemplate(LettuceConnectionFactory redisConnectionFactory) {
