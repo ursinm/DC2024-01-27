023f6d7a493af24c4581b3960a8050e6b795d591
l9sik <64441809+l9sik@users.noreply.github.com>
2024-05-02 15:10:22 +0300

    fix: add null check for country

--- 151002/Poluectov/Rv-project/publisher/src/main/java/com/poluectov/rvproject/controller/crud/MessageController.java
+++ 151002/Poluectov/Rv-project/publisher/src/main/java/com/poluectov/rvproject/controller/crud/MessageController.java
@@ -5,6 +5,8 @@ import com.poluectov.rvproject.dto.message.MessageResponseTo;
 import com.poluectov.rvproject.model.Message;
 import com.poluectov.rvproject.service.message.MessageService;
 import com.poluectov.rvproject.utils.modelassembler.MessageModelAssembler;
+import io.micrometer.common.util.StringUtils;
+import io.netty.util.internal.StringUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -16,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/messages")
 public class MessageController extends CommonRESTController<Message, MessageRequestTo, MessageResponseTo> {
-    public MessageController(@Qualifier("messageService") MessageService service,
+    public MessageController(@Qualifier("cachedMessageService") MessageService service,
                              MessageModelAssembler assembler) {
         super(service, assembler::toModel);
     }
@@ -25,7 +27,12 @@ public class MessageController extends CommonRESTController<Message, MessageRequ
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<?> newEntity(@RequestBody MessageRequestTo request, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        request.setCountry(httpServletRequest.getLocale().getCountry());
+        String country = httpServletRequest.getLocale().getCountry();
+        if (StringUtils.isEmpty(country)) {
+            country = "locale";
+        }
+
+        request.setCountry(country);
 
         return super.newEntity(request, httpServletRequest, httpServletResponse);
     }
