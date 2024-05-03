a21f19e0f584d1cc0304f0f939c897bf3f870e67
Maxin4rm <92393477+Maxin4rm@users.noreply.github.com>
2024-05-03 11:29:22 +0300

    Add files via upload

---
+++ 151002/Kuvshinov/lab4/Lab4.Publisher/Repositories/Implementations/LabelSqlRepository.cs
@@ -0,0 +1,9 @@
+﻿using Lab4.Publisher.Data;
+using Lab4.Publisher.Models;
+using Lab4.Publisher.Repositories.Interfaces;
+
+namespace Lab4.Publisher.Repositories.Implementations;
+
+public class StickerSqlRepository(AppDbContext context) : BaseSqlRepository<Sticker>(context), IStickerRepository
+{
+}
